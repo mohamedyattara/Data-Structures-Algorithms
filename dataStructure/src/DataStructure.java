ac757e521353e8e402e7373891dce7148841cf61
yatta <yatta@DESKTOP-BPSNP8E.lan>
2026-01-03 18:37:48 -0500

    First Commit, Project Structure

---
+++ dataStructure/src/DataStructure.java
@@ -0,0 +1,26 @@
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class DataStructure {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private DataStructure() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
